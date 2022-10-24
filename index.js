d1096f03f03f5dde2b2b2aa1ad01a38f30ebd198
Elavarasan selvaraj <elavarasan.selvaraj@aroopatech.com>
2022-10-24 19:39:01 +0530

    update package version and fixed npx issue

--- index.js
+++ index.js
@@ -1,44 +1,42 @@
+#!/usr/bin/env node
+
 const fs = require("fs");
 
-const path = "./app/model";
-
-const pathCreation = async(path)=>{
-    
-fs.access(path, (error) => {
-
-    
+const pathCreation = async (path) => {
+  fs.access(path, (error) => {
     if (error) {
-        fs.mkdir(path, (error) => {
+      fs.mkdir(path, (error) => {
         if (error) {
-            console.log(error);
-        } else {
-            console.log("New Directory created successfully !!");
-        }
-        });
+          console.log(error);
+        } 
+      });
     } else {
-        console.log("Given Directory already exists !!");
+      console.log("Given Directory already exists !!");
     }
-    });
-}
+  });
+  
+};
 
-const backend_folder_structure = async ()=>{
-    const paths = ['app','./app/controller','./app/model','./app/route'];
+const backend_folder_structure = async () => {
+  const paths = ["app", "./app/controller", "./app/model", "./app/route"];
 
-    for (let count = 0; count < paths.length; count++) {
-       await pathCreation(paths[count]);
-    }
-    const sampleCode = `
+  for (let count = 0; count < paths.length; count++) {
+    await pathCreation(paths[count]);
+  }
+  const sampleCode = `
     const http = require('http');
     http.createServer(function (req, res) {
         res.write('All the best for project'); 
         res.end(); 
-      }).listen(8080); 
+      }).listen(8080,()=>{
+        console.log("server is Running at 8080");
+      }); 
     `;
-    
-    fs.appendFile('server.js', sampleCode, function (err) {
-        if (err) throw err;
-        console.log('File is created successfully.');
-    });
-    
-}
+
+  fs.appendFile("server.js", sampleCode, function (err) {
+    if (err) throw err;
+    console.log("Express Backend Structure Created")
+  });
+};
+
 backend_folder_structure()
